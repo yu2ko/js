2c8774b168e6af038ef29b1c4dd96884d34b931f
Yurii Kuzmenko <yykksua@ymail.com>
2023-11-02 10:57:27 +0200

    js lessons update

---
+++ index3.js
@@ -0,0 +1,48 @@
+// let n = -10.55;
+
+// let n = -99999999999999999999999999999999999 / "string";
+let n = -99999999999999999999999999999999999n;
+let b = NaN;
+let c = -Infinity;
+
+// console.log(n, b, c);
+// console.log(1 / 0);
+// console.log(-1 / 0);
+
+console.log(99999999999999 > Infinity);
+
+const t = "hello";
+const p = '"hello"';
+const l = `hello ${t} ${2 + 2}`;
+
+console.log(l);
+
+let yes = true;
+let no = false;
+
+let isAdmin = true;
+
+// let none = null;
+
+let none = null;
+
+console.log(none);
+
+console.log(typeof n);
+
+// console.log(isNaN(n));
+console.log(isNaN(yes));
+console.log(isNaN(b));
+
+console.log(isFinite(100));
+console.log(isFinite(c));
+
+// помилка у js:
+console.log(typeof null);
+// =========================
+
+console.log(typeof true);
+
+console.log(typeof t);
+console.log(typeof p);
+console.log(typeof l);
